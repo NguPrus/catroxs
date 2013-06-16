b568708c50cd47c4231a5aa73747400d8ba2d688
agssbuzz <batalion.jalak.harupat@gmail.com>
2013-06-17 05:00:29 +0700

    [Dev] Ditambahkan file yang hilang

---
+++ closed.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @package     JohnCMS
+ * @link        http://johncms.com
+ * @copyright   Copyright (C) 2008-2011 JohnCMS Community
+ * @license     LICENSE.txt (see attached file)
+ * @version     VERSION.txt (see attached file)
+ * @author      http://johncms.com/about
+ * @dev		    agssbuzz@catroxs.org
+			    http://www.catroxs.org
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Catroxs.org ! работы на сайте</title>
+  <!-- <title>Catroxs.org ! under construction</title>  -->
+</head>
+
+<body>
+
+<?php
+
+// English version
+
+echo '<center><img src="theme/web/images/logo.gif" alt="JohnCMS" /><br />
+<span style="color:red"><b>Attention!</b></span><br />
+<img src="images/red.gif" width="16" height="16" alt="" /> Site under construction.<br />Please, try again later</center><br /></center> ';
+?>
+
+</body>
+
+</html>
